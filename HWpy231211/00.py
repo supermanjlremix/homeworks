d09156d15b1211fd9cbd744994a87a858c1f2b7a
Argyn <81853521+supermanjlremix@users.noreply.github.com>
2023-12-28 16:37:24 +0600

    1

--- HWpy231211/00.py
+++ HWpy231211/00.py
@@ -11,7 +11,11 @@ for i in range(len(list_format)-1):
     if list_format[i][-1] in ".!?":
         list_format[i+1] = list_format[i+1].capitalize()
 
-width = int(input("Введите ширину строки: "))
+try:
+    width = int(input("Введите ширину строки: "))
+except ValueError:
+    print("Введите цифру для длины строк!")
+
 string_list = []
 string = "   "
 for word in list_format:
@@ -22,9 +26,8 @@ for word in list_format:
         string = word
 string_list += [string]
 
-for row in string_list:
+for row in string_list: #не могу сделать пробелы по очередности между словами и чтобы абзац не учитывал
     row = list(row)
-    print(row, len(row))
     for symbol in row:
         if len(row) == width:
             break
@@ -32,30 +35,4 @@ for row in string_list:
             row.insert(row.index(symbol), " ")
     string = "".join(row)
     print(string)
-    print(row, len(row))
 
-    # for symbol in row:
-    #     spacer = " "
-    #     if len(row) == width:
-    #         break
-    #     elif symbol == spacer and row[row.index(symbol)-1] != " ":
-    #         # while len(row) != width or "  " in row:
-    #         row = list(row)
-    #         row.insert(row.index(symbol),spacer)
-    #         print(row, len(row))
-
-                # row = row[:row.index(symbol)]+ "  " + row[row.index(symbol)+1:]
-    # string = "".join(row)
-    # print(string)
-    # print(row, len(row))
-
-
-
-
-    # for symbol in row:
-    #     if len(row) == width:
-    #         break
-    #     elif len(row) < width and symbol == " ":
-    #         # row = row[:row.index(symbol)-1]+ "  " + row[row.index(symbol):]
-    #         row = row.replace(" ", "  ")
-    #         print(row, len(row))
