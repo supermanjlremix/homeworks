65fbbf11ad9d19a6fbf6ff021297c702bd96b2c5
Argyn <81853521+supermanjlremix@users.noreply.github.com>
2024-01-12 02:21:21 +0600

    1

--- HWpy231219/00.py
+++ HWpy231219/00.py
@@ -28,11 +28,14 @@ def fireEmployee(id: int) -> dict:
         return {"id": -1, "errorDescription": str(errorDescription)}
 
 
-def getEmployeeId(name: str) -> int:
+def getEmployeeId(name: str) -> int|set:
+    id_set = set() 
     for employee in database:
         for key, value in employee.items():
             if value == name:
-                return employee["id"]
+                id_set.add(employee["id"])
+    if id_set:
+        return id_set
     return -1
 
 
@@ -45,6 +48,7 @@ def showFiredEmployees() -> list:
     for employee in database:
         if employee["firedDate"] is not None:
             firedEmployees += [employee]
+            break
     return firedEmployees
 
 
@@ -55,7 +59,10 @@ def getSalaryStats() -> dict:
             salaries += [employee["salary"]]
     salaries = sorted(salaries)
     average_salary = sum(salaries)/len(salaries)
-    median_salary = (salaries[int(len(salaries)/2)] + salaries[int(len(salaries)/2)-1]) / 2 if len(salaries) % 2 == 0 else salaries[int(len(salaries)/2)]
+    if len(salaries) % 2 == 0:
+        median_salary = (salaries[int(len(salaries)/2)] + salaries[int(len(salaries)/2)-1]) / 2
+    else:
+        median_salary = salaries[int(len(salaries)/2)]
     stats = {"Общий размер зарплатного фонда компании": sum(salaries), "Максимальная зарплата": max(salaries), "Минимальная зарплата": min(salaries), "Средняя зарплата": average_salary, "Медианная зарплата": median_salary}
     return stats
 
@@ -71,8 +78,8 @@ print(fireEmployee(0))
 print(fireEmployee(4))
 print(getEmployeeId("Ғалымжан"))
 print(getEmployeeId("Нежельский"))
+print(getEmployeeId("Тимур"))
 print(getEmployeeId("Мадина"))
 print(getEmployeeRecord(2))
-print(database)
 print(showFiredEmployees())
 print(getSalaryStats())
