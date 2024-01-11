3d89ff467ef9ac42f008dcfc439288880230deaf
Argyn <81853521+supermanjlremix@users.noreply.github.com>
2024-01-12 03:09:43 +0600

    Update 02.py

--- HWpy231218/02.py
+++ HWpy231218/02.py
@@ -1,5 +1,6 @@
+from math import pi
+
 def get_barrel_capacity(d: int|float, h:int|float) -> float:
-    pi = 3.14
     barrel_capacity = ((h * 1000) * pi * (d * 1000) ** 2 / 4)  / 1000000
     return barrel_capacity
 
