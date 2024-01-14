ceaabacc2ddc658f15953780c7464143a30e9b44
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2024-01-14 23:04:01 +0600

    Проверено дз от 18.12
    Комментарий к коду

--- HWpy231218/00.py
+++ HWpy231218/00.py
@@ -1,4 +1,5 @@
 def get_sum_numbers(number1: int, number2: int, number3: int) -> str:
+    #VN:                         здесь тоже должен быть тип int  ^^^^
     result = str(number1) + str(number2) + str(number3)
     return result
 
