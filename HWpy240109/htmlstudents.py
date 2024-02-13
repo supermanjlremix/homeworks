992e0c6f3906b54f9fb20351819a93daf2b9b652
Argyn <81853521+supermanjlremix@users.noreply.github.com>
2024-02-13 14:41:46 +0600

    1

--- HWpy240109/htmlstudents.py
+++ HWpy240109/htmlstudents.py
@@ -1,4 +1,4 @@
-def get_html_students(data_students: tuple, course: str) -> tuple:
+def get_html_students(data_students: tuple, course: str='HTML') -> tuple:
     html_top_students = tuple(
         map(lambda student: student['name'],
             filter(lambda student: student['course'] == course and 
